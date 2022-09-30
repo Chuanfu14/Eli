cc96ff91c315a3f85294f603940f963ac293f1eb
HZhao18 <77083194+HZhao18@users.noreply.github.com>
2022-09-30 12:27:13 -0400

    Update main.py
    Math Problems v2
    -Added input validations
    -Score percentage

--- main.py
+++ main.py
@@ -1,168 +1,143 @@
 import random
 import math
 
-TOTAL_PROBLEMS = 5
-MIN_THRESHOLD = 0
-MAX_THRESHOLD = 10
-num_correct = 0
+min_threshold = 0
+max_threshold = 10
 
 
+# generates user prompt
 def user_prompt():
-    """
-    This function returns the prompt that the user is presented with to choose
-    what kind of problem they want to practice.
-    """
-    return """
+    print('''
     Enter the number of the type of arithmetic problem you want to practice.
-    1) Addition
-    2) Subtraction
-    3) Exponential Function
-    4) Pythagorean Theorem
-    """
+    1) Addition 2) Subtraction 3) Exponential Function 4) Pythagorean Theorem
+    ''')
 
 
+# user inputs how many questions they want to practice
+def num_of_questions():
+    while True:
+        try:
+            total_problems = int(input("How many questions do you want to practice? "))
+        except ValueError:
+            print("Please enter a number")
+        else:
+            return total_problems
+
+
+def user_choice():
+    while True:
+        try:
+            user_input = int(input("Your choice: "))
+        except ValueError:
+            print("Please enter a number to select what type of math questions you want to practice.")
+        else:
+            return user_input
+
+
+# generates random numbers from 0 to 10
 def generate_input():
-    """
-    This function generates a random number to act as input for an
-    arithmetic problem.
-    """
-    return random.randint(MIN_THRESHOLD, MAX_THRESHOLD)
+    return random.randint(min_threshold, max_threshold)
 
 
-def create_arithmetic_problem_prompt(a, b, operator):
-    """
-    This function takes in two numbers and an operator and returns a string
-    that is built from these three pieces of information
-    """
-    return "What is " + str(a) + " " + operator + " " + str(b) + "?"
+# generate math question based on user selection
+def create_arithmetic_problem_prompt(a, b, user_input):
+    if user_input == 1:
+        user_input = '+'
+        print("What is " + str(a) + " " + str(user_input) + " " + str(b) + "?")
+    elif user_input == 2:
+        user_input = '-'
+        print("What is " + str(a) + " " + str(user_input) + " " + str(b) + "?")
+    elif user_input == 3:
+        print("What is " + str(a) + " to the power of " + str(b) + " ?")
+    elif user_input == 4:
+        print("What does " + (str(a)) + " squared + " + str(b) + " squared make c, the hypotenuse ?")
 
 
-def create_pythagorean_problem_prompt(a, b):
-    """
-    This function takes two numbers and returns the value deemed as the hypotenuse
-    """
-    return "What does " + (str(a)) + " squared + " + str(b) + " squared make c, the hypotenuse ?"
-
-
-def create_exponential_problem_prompt(a, b):
-    """
-    This function takes two numbers and returns the exponential value
-    """
-    return "What is " + str(a) + " to the power of " + str(b) + " ?"
-
-
-def assess_user_response(user_answer, true_answer):
-    """
-    This function takes in two answers, one of which is provided by
-    the user and one of which is the correct answer. It generates an
-    appropriate response depending on whether the user answers
-    matches the correct answer.
-    """
-    if user_answer == true_answer:
-        print("Correct, great work!")
-        return True
-    else:
-        print("Incorrect. The expected answer was " + str(true_answer))
-        return False
-
-
+# addition
 def addition(a, b):
-    """
-    Simple function to encapsulate addition
-    """
     return a + b
 
 
-def user_answer():
-    """
-    input by user
-    """
-    int(input("Your answer: "))
-
-
-def true_answer():
-    """
-    calculated actual answer
-    """
-
-
-def check(operation):
-    """
-    checking for if user input is correct
-    """
-    user_answer = int(input("Your answer: "))
-    true_answer = {operation}(a, b)
-    correct = assess_user_response(user_answer, true_answer)
-    if correct:
-        num_correct += 1
-    num_problems_done += 1
-
-
+# subtraction
 def subtraction(a, b):
-    """
-    Simple function to encapsulate subtraction
-    """
     return a - b
 
 
+# exponential
 def exponential(a, b):
-    """
-    Simple function to encapsulate exponential problems
-    """
     return a ** b
 
 
+# pythagorean
 def pythagorean(a, b):
-    """
-    Simple function to encapsulate the pythagorean theorem
-    """
     return math.sqrt(a ** 2 + b ** 2)
 
 
-def main():
-    num_problems_done = 0
+# this function ensures user only input numbers
+def validate_user_answer():
+    while True:
+        try:
+            user_answer = int(input("Your answer is: "))
+        except ValueError:
+            print("Bad Input! Please enter your answer in numbers.")
+        else:
+            return user_answer
+
+
+# compare user answer and the true answer based on their chosen operation
+def check_answer(user_input, a, b, user_answer):
     num_correct = 0
-    while num_problems_done < TOTAL_PROBLEMS:
-        print(user_prompt())
-        choice = int(input("Your choice: "))
+    problems_solved = 0
+    if user_input == 1:
+        if addition(a, b) == user_answer:
+            print('Correct!')
+            return True
+        else:
+            print("Incorrect. The expected answer was " + str(addition(a, b)))
+            return False
+    elif user_input == 2:
+        if subtraction(a, b) == user_answer:
+            print('Correct!')
+            return True
+        else:
+            print("Incorrect. The expected answer was " + str(subtraction(a, b)))
+            return False
+    elif user_input == 3:
+        if exponential(a, b) == user_answer:
+            print('Correct!')
+            return True
+        else:
+            print("Incorrect. The expected answer was " + str(exponential(a, b)))
+            return False
+    elif user_input == 4:
+        if pythagorean(a, b) == user_answer:
+            print('Correct!')
+            return True
+        else:
+            print("Incorrect. The expected answer was " + str(pythagorean(a, b)))
+            return False
+
+
+def main():
+    num_correct = 0  # initiate counter
+    problem_solved = 0  # initiate counter
+    for x in range(num_of_questions()):
+        (user_prompt())
+        user_input = user_choice()
         a = generate_input()
         b = generate_input()
-        if choice == 1:
-            print(create_arithmetic_problem_prompt(a, b, "+"))
-            user_answer = int(input("Your answer: "))
-            true_answer = addition(a, b)
-            correct = assess_user_response(user_answer, true_answer)
-            if correct:
-                num_correct += 1
-            num_problems_done += 1
-        elif choice == 2:
-            print(create_arithmetic_problem_prompt(a, b, "-"))
-            user_answer = int(input("Your answer: "))
-            true_answer = subtraction(a, b)
-            correct = assess_user_response(user_answer, true_answer)
-            if correct:
-                num_correct += 1
-            num_problems_done += 1
-        elif choice == 3:
-            print(create_exponential_problem_prompt(a, b))
-            user_answer = int(input("Your answer: "))
-            true_answer = exponential(a, b)
-            correct = assess_user_response(user_answer, true_answer)
-            if correct:
-                num_correct += 1
-            num_problems_done += 1
-        elif choice == 4:
-            print(create_pythagorean_problem_prompt(a, b, ))
-            user_answer = int(input("Your answer: "))
-            true_answer = pythagorean(a, b)
-            correct = assess_user_response(user_answer, true_answer)
-            if correct:
-                num_correct += 1
-            num_problems_done += 1
+        create_arithmetic_problem_prompt(a, b, user_input)
+        user_answer = validate_user_answer()
+        if not check_answer(user_input, a, b, user_answer):
+            problem_solved += 1
         else:
-            print("Sorry, you didn't choose a valid option!")
-    print("You got " + str(num_correct) + " out of 5 problems correct!")
+            num_correct += 1
+            problem_solved += 1
+            print("You got", num_correct, "out of", problem_solved, "questions correct.")
+
+    print("Your score is", num_correct / problem_solved * 100,"%")
 
 
 if __name__ == "__main__":
     main()
+
